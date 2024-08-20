ec9a5eeb3645371a61cae785b0360ea7c75c511b
LENOVO <sworupdhakal88@gmail.com>
2024-08-20 13:22:58 +0545

    j

--- src/models/Measurement.java
+++ src/models/Measurement.java
@@ -1,2 +1,21 @@
-package models;public class Measurement {
+package models;
+
+public class Measurement {
+    public Integer val;
+
+    public Measurement(Integer val) {
+        this.val = val;
+    }
+    public int getCm(){
+        return val * 100;
+    }
+    public double getKm(){
+        return val / 1000.0;
+    }
+    public int getMm(){
+        return val * 1000;
+    }
+    public int getDm(){
+        return val * 10;
+    }
 }
